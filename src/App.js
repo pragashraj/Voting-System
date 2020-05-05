2fb9cb8f6833ab98e9e36ef60aeefe2ce0d7c55e
S.Pragashraj <rajpragashraj30@gmail.com>
2020-05-05 07:29:57 +0530

    react-router-dom package added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import Home from './pages/Home'
 
 const App=()=> {
   return (
     <div className="App">
-      
+        <Home/>
     </div>
   )
 }
